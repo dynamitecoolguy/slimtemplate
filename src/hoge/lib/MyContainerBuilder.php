71c861e05fc3840d082026090f0dcee954fc806f
Shinji Kodama <kodama@hand.co.jp>
2019-10-18 16:52:26 +0900

    secretsmanager対応改めsystem manager対応

--- src/hoge/lib/MyContainerBuilder.php
+++ src/hoge/lib/MyContainerBuilder.php
@@ -2,12 +2,10 @@
 
 namespace Hoge;
 
+use Aws\Credentials\CredentialProvider;
 use Aws\Exception\AwsException;
 use Aws\Sdk;
-use Aws\SecretsManager\SecretsManagerClient;
-use Aws\WorkSpaces\Exception\WorkSpacesException;
 use PDO;
-use phpDocumentor\Reflection\Types\Mixed_;
 use Psr\Container\ContainerInterface;
 
 use DI\Container;
@@ -17,20 +15,32 @@ use Redis;
 class MyContainerBuilder extends ContainerBuilder
 {
     /**
-     * Secrets Manager上の設定格納キー
+     * 設定ファイルのパス
      */
-    const SECRET_VALUE_KEY = 'slimtemplate_secret';
+    const SETTINGS_YAML = '/usr/local/etc/myapp/settings.yml';
 
     /**
-     * localにcacheするsecretのTTL
+     * 設定ファイルのパス
      */
-    const SECRET_TTL = 60;
+    const SSM_CREDENTIALS_PROFILE = 'default';
+    const SSM_CREDENTIALS_PATH = '/usr/local/etc/myapp/credentials/ssm';
+
+    /**
+     * localにcacheするparameterのTTL
+     */
+    const PARAMETER_TTL = 60;
 
     /**
      * @var array
      * 取得済みの値
      */
     private $replacedText = [];
+
+    /**
+     * @var array
+     * settings.yamlの内容
+     */
+    private $settings = [];
 
     /**
      * MyContainerBuilder constructor.
@@ -40,6 +50,8 @@ class MyContainerBuilder extends ContainerBuilder
     {
         parent::__construct($containerClass);
 
+        $this->settings = yaml_parse_file(static::SETTINGS_YAML);
+
         $this->addDefaultDefinitions();
     }
 
@@ -48,17 +60,12 @@ class MyContainerBuilder extends ContainerBuilder
      */
     private function addDefaultDefinitions(): void
     {
-        // 設定用YAMLファイル読み込み
-        // 設定ファイルはdockerによって/var/www下にコピーされる
-        $parsed = yaml_parse_file(__DIR__ . '/../../settings.yml');
-
         $this->addDefinitions([
-            'settings' => $parsed,
+            'settings' => $this->settings,
             'userdb' => function (ContainerInterface $container)
             {
-                $setting = $this->getSettingArray($container, 'userdb');
-                $dsn = 'mysql:host=' . $this->getSettingValue($setting, 'host')
-                    . ';dbname=' . $this->getSettingValue($setting, 'dbname');
+                $dsn = 'mysql:host=' . $this->getSettingValue('userdb', 'host')
+                    . ';dbname=' . $this->getSettingValue('userdb', 'dbname');
                 $options = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -66,16 +73,15 @@ class MyContainerBuilder extends ContainerBuilder
                 ];
                 return new PDO(
                     $dsn,
-                    $this->getSettingValue($setting, 'user'),
-                    $this->getSettingValue($setting, 'password'),
+                    $this->getSettingValue('userdb', 'user'),
+                    $this->getSettingValue('userdb', 'password'),
                     $options
                 );
             },
             'logdb' => function (ContainerInterface $container)
             {
-                $setting = $this->getSettingArray($container, 'logdb');
-                $dsn = 'pgsql:host=' . $this->getSettingValue($setting, 'host')
-                    . ';dbname=' . $this->getSettingValue($setting, 'dbname');
+                $dsn = 'pgsql:host=' . $this->getSettingValue('logdb', 'host')
+                    . ';dbname=' . $this->getSettingValue('logdb', 'dbname');
                 $options = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -83,30 +89,28 @@ class MyContainerBuilder extends ContainerBuilder
                 ];
                 return new PDO(
                     $dsn,
-                    $this->getSettingValue($setting, 'user'),
-                    $this->getSettingValue($setting, 'password'),
+                    $this->getSettingValue('logdb', 'user'),
+                    $this->getSettingValue('logdb', 'password'),
                     $options
                 );
             },
             'redis' => function (ContainerInterface $container)
             {
-                $setting = $this->getSettingArray($container, 'redis');
                 $redis = new Redis();
-                $redis->connect($this->getSettingValue($setting, 'host'));
+                $redis->connect($this->getSettingValue('redis', 'host'));
                 $redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);
 
                 return $redis;
             },
             'dynamodb' => function (ContainerInterface $container)
             {
-                $setting = $this->getSettingArray($container, 'dynamodb');
                 $sdk = new Sdk([
-                    'endpoint' => $this->getSettingValue($setting, 'endpoint'),
-                    'region' => $this->getSettingValue($setting, 'region'),
+                    'endpoint' => $this->getSettingValue('dynamodb', 'endpoint'),
+                    'region' => $this->getSettingValue('dynamodb', 'region'),
                     'version' => '2012-08-10',
                     'credentials' => [
-                        'key' => $this->getSettingValue($setting, 'key'),
-                        'secret' => $this->getSettingValue($setting, 'secret')
+                        'key' => $this->getSettingValue('dynamodb', 'key'),
+                        'secret' => $this->getSettingValue('dynamodb', 'secret')
                     ]
                 ]);
 
@@ -114,14 +118,13 @@ class MyContainerBuilder extends ContainerBuilder
             },
             'storage' => function (ContainerInterface $container)
             {
-                $setting = $this->getSettingArray($container, 'storage');
                 $sdk = new Sdk([
-                    'endpoint' => $this->getSettingValue($setting, 'endpoint'),
-                    'region' => $this->getSettingValue($setting, 'region'),
+                    'endpoint' => $this->getSettingValue('storage', 'endpoint'),
+                    'region' => $this->getSettingValue('storage', 'region'),
                     'version' => '2006-03-01',
                     'credentials' => [
-                        'key' => $this->getSettingValue($setting, 'key'),
-                        'secret' => $this->getSettingValue($setting, 'secret')
+                        'key' => $this->getSettingValue('storage', 'key'),
+                        'secret' => $this->getSettingValue('storage', 'secret')
                     ],
                     'use_path_style_endpoint' => true
                 ]);
@@ -131,33 +134,18 @@ class MyContainerBuilder extends ContainerBuilder
     }
 
     /**
-     * settings.ymlファイルの中から指定されたカテゴリの設定を返す
-     * @param ContainerInterface $container
+     * 指定されたカテゴリ内のキーの値を取得する
      * @param string $category
-     * @return array
-     */
-    private function getSettingArray(ContainerInterface $container, string $category): array
-    {
-        $settingArray = $container->get('settings');
-        if (!isset($settingArray[$category])) {
-            return []; // TODO: 本当はException
-        }
-        return $settingArray[$category];
-    }
-
-    /**
-     * getSettingArrayで取得した配列から、指定されたキーの値を取得する
-     * @param array $setting
      * @param string $key
      * @return mixed
      */
-    private function getSettingValue(array $setting, string $key)
+    private function getSettingValue(string $category, string $key)
     {
-        if (!isset($setting[$key])) {
+        if (!isset($this->settings[$category]) || !isset($this->settings[$category][$key])) {
             return false; // TODO: 本当はException
         }
+        $value = $this->settings[$category][$key];
 
-        $value = $setting[$key];
         if (strpos($value, '$$') === false) {
             return $value; // 置換の必要が無ければその値のまま返す
         }
@@ -187,26 +175,45 @@ class MyContainerBuilder extends ContainerBuilder
             return $fetchedValue;
         }
 
-        // AWS Secret Managerから取得 (json形式)
-        $client = new SecretsManagerClient([
-            'profile' => 'default',
-            'version' => '2017-10-17',
-            'region' => 'ap-northeast-1'
-        ]);
+        // SSMサービスへの認証
+        $provider = CredentialProvider::ini(
+            static::SSM_CREDENTIALS_PROFILE,
+            static::SSM_CREDENTIALS_PATH
+        );
+        $memoizedProvider = CredentialProvider::memoize($provider);
 
+        // SSM Clientの取得
+        $ssmSetting = $this->settings['ssm'];
+        $sdk = new Sdk([
+            'endpoint' => $ssmSetting['endpoint'],
+            'region' => $ssmSetting['region'],
+            'version' => '2014-11-06',
+            'credentials' => $memoizedProvider
+        ]);
+        $ssm = $sdk->createSsm();
+
+        // settingsファイル内のすべての$$キーをリストアップ
+        $keys = [];
+        foreach ($this->settings as $settingArray) {
+            foreach ($settingArray as $key => $value) {
+                if (preg_match('/\$\$(.*)\$\$/', $value, $matches)) { // {{KEY_NAME}}か?
+                    $keys[] = $matches[1];
+                }
+            }
+        }
         try {
-            $result = $client->getSecretValue([
-                'SecretId' => static::SECRET_VALUE_KEY
+            $result = $ssm->getParameters([
+                'Names' => $keys
             ]);
         } catch (AwsException $e) {
+            var_dump($e);
             return false;  // TODO: 本当はException
         }
-        if (isset($result['SecretString'])) {
-            $secret = $result['SecretString'];
-        } else {
-            $secret = base64_decode($result['SecretBinary']);
-        }
 
+        var_dump($result);
+
+        return false;
+/*
         // jsonデータを展開し、apcuとローカルに格納する
         foreach (json_decode($secret, true) as $key => $value) {
             if (!apcu_exists($key)) {
@@ -220,5 +227,7 @@ class MyContainerBuilder extends ContainerBuilder
         }
 
         return $this->replacedText[$original];
+*/
     }
 }
+
